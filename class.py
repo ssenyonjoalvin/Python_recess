05ba61ebab6febe6ffd9df5098ebb73a6dfd62fb
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2024-06-05 13:07:54 +0300

    added comments

--- class.py
+++ class.py
@@ -1,10 +1,13 @@
 # this line  displays the word test in terninal when the code is run 
 print("test")
+
  #variables 
 x = 5
 y = 10
 print("sum = " , x + y)
-#to enable users input data and print it on the screen
+
+#to enable users input data and print it on the screen 
+#the input function helps users to input data to the program
 name = input("enter name ")
 color = input("enter color ")
 print(name , " likes " , color)
