cea8a360375f7ba06cddf67784021505173e6de6
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2024-06-05 12:45:16 +0300

    my first programm in python.

---
+++ class.py
@@ -0,0 +1,10 @@
+# this line  displays the word test in terninal when the code is run 
+print("test")
+ #variables 
+x = 5
+y = 10
+print("sum = " , x + y)
+#to enable users input data and print it on the screen
+name = input("enter name ")
+color = input("enter color ")
+print(name , " likes " , color)
