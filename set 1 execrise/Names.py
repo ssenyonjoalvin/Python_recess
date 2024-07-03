1f7e8a3b0d32e2274a5281f8292022a6a4db4237
alvinssenyonjo <alvinssenyonjo9@gmail.com>
2024-07-03 11:38:35 +0300

    updated with day12

--- set 1 execrise/Names.py
+++
@@ -1,9 +0,0 @@
-group_names = {
-    "SSENYONJO ALVIN " : "22/U/6952",
-    "AKOL PAUL" : "22/U/22453",
-    "AHABWE DERRICK" : "22/U/5665",
-    "NAKASUJJA GERTRUDE" : "21/U/07445/EVE",
-    "MUSIMENTA CISSYLYN" : "22/U/6402"
- 
-}
-print(group_names)
